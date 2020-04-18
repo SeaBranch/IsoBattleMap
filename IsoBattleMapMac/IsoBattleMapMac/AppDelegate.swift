a66fc7f98984cc9c6651edfe8f5d9f5d806d9f88
Nathan Sjoquist <nwsjoquist@gmail.com>
2020-04-18 14:10:15 -0400

    adding Mac App Starter

---
+++ IsoBattleMapMac/IsoBattleMapMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IsoBattleMapMac
+//
+//  Created by Nathan Sjoquist on 4/18/20.
+//  Copyright © 2020 Nathan Sjoquist. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
